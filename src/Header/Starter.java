c41b13564b79303c1145410f6e5b77e2b7727e93
Harish <harishmintm@gmail.com>
2018-05-30 01:31:32 +0530

    First set of files

---
+++ src/Header/Starter.java
@@ -0,0 +1,53 @@
+package Header;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import Page_Objects.*;
+
+public class Starter {
+
+	public WebDriver driver;
+	public Properties p = new Properties();
+	
+	@BeforeTest
+	public void setup() throws IOException {
+		
+		FileInputStream f = new FileInputStream("/home/harish/Desktop/\\/SharafDG_clicks/src/default.properties");
+		p.load(f);
+		
+		System.setProperty("webdriver.chrome.driver", p.getProperty("cpath"));
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void login() {
+		
+		driver.get(p.getProperty("url"));
+		System.out.println(driver.getTitle());
+		
+		Login_Page lp = new Login_Page(driver);
+		
+		lp.email().sendKeys(p.getProperty("email"));
+		
+		lp.password().sendKeys(p.getProperty("pass"));
+		
+		lp.submit().click();
+		
+		
+	}
+	
+	@AfterTest
+	public void teardown() {
+		
+		driver.close();
+	}
+	
+}
---
+++ src/Page_Objects/Login_Page.java
@@ -0,0 +1,41 @@
+package Page_Objects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Login_Page {
+
+	WebDriver driver;
+
+	public Login_Page(WebDriver driver) {
+
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(name="email")
+	WebElement email;
+
+	@FindBy(name="password")
+	WebElement password;
+
+	@FindBy(xpath="//div//form/input[@value=\"Sign in\"]")
+	WebElement submit;
+
+	public WebElement email() {
+
+		return email;
+	}
+
+	public WebElement password() {
+
+		return password;
+	}
+
+	public WebElement submit() {
+
+		return submit;
+	}
+}
